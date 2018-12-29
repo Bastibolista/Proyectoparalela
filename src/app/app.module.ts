e1c66a1b2d28d9cb33df79df10050d431e37a272
Fleon <fabian_eal@hotmail.cl>
2018-12-29 04:18:08 -0300

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { TeacherComponent } from './components/teacher/teacher.component';
+import { StudentComponent } from './components/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    TeacherComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { TeacherComponent } from './components/teacher/teacher.component';
+import { StudentComponent } from './components/student/student.component';
+
+const appRoutes: Routes = [
+	{path: '', component: LoginComponent},
+	{path: 'home', component: HomeComponent},
+	{path: 'teacher', component: TeacherComponent},
+	{path: 'student', component: StudentComponent},
+	{path: '**', component: LoginComponent},
+
+];
+
+export const appRoutingProviders: any[]=[];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
